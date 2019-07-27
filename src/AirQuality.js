c667a58f9647829863c45019e76fcfe75232ab9f
Zac Xu <zacxu@Zacs-MacBook.local>
2019-07-27 16:47:47 +1000

    add search feature

--- src/AirQuality.js
+++ src/AirQuality.js
@@ -1,29 +1,44 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import AirQualityListItem from './AirQualityListItem';
 import './AirQuality.css';
 
-const URL = 'https://api.openaq.org/v1/measurements?country=AU';
+const URL = 'https://api.openaq.org/v1/measurements';
 
 class AirQuality extends Component {
     state = {
         data: null,
+        input: '',
+        isLoading: false
     };
 
-    // 挂载函数 fetch是异步
-    componentDidMount() {
-        //叫promise上的Function
-        //正常运行用then
-        fetch(URL)
-            .then(response => response.json()) //fetch函数调用json， https://developer.mozilla.org/en-US/docs/Web/API/Body/json
-            .then(response => this.setState({
-                data: response,
-            }))    //链式调用不加分号,results数组
-            .catch(error => console.log(error));
-        //catch 捕获异常
+    handleInput = (e) => {
+        this.setState({
+            input: e.target.value,
+        });
     }
 
     handleClick = () => {
-        alert("See");
+        this.setState({
+            isLoading: true,
+        });
+        //叫promise上的Function
+        //正常运行用then
+        axios.get(URL, {
+            params: {
+                country: this.state.input,
+            }
+        })
+            .then(response => {
+                this.setState({
+                    isLoading: false,
+                    data: response.data,
+                });
+            })    //链式调用不加分号,results数组
+            .catch(error => console.log(error));
+        //catch 捕获异常
+
     }//调用成员变量，值是函数，react专属
 
     render() {
@@ -31,17 +46,20 @@ class AirQuality extends Component {
         return <div className="AirQualityList">
             <h1>Your Weather Station</h1>
 
-            <input type="text" />
+            <input type="text" onInput={this.handleInput} />
 
-            <button type="submit" onClick={this.handleClick}>Search</button>
+            <button type="submit" onClick={this.handleClick}>
+                {this.state.isLoading ? 'Loading…' : 'Search'}
+            </button>
 
 
             {this.state.data && <div> {/* 保护性检查 */}
+
                 {this.state.data.results.map(item => /* 把每一组数据循环调用 */
                     <AirQualityListItem
                         location={item.location}
                         parameter={item.parameter}
-                        value={item.vailue}
+                        value={item.value}
                     />
                 )}
             </div>}
